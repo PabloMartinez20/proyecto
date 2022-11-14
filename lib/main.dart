ac6f0a0a9f825e7f27edf6d9a6dda35597b10d56
PabloMartinez20 <ezelokopablo@gmail.com>
2022-11-14 10:23:49 -0300

    Se agrega proyecto

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_insta_clone/insta_home.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Instagram',
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(
+          primarySwatch: Colors.blue,
+          primaryColor: Colors.black,
+          buttonColor: Colors.black,
+          primaryIconTheme: IconThemeData(color: Colors.black),
+          primaryTextTheme: TextTheme(
+              bodyText1: TextStyle(color: Colors.black, fontFamily: "Aveny")),
+          textTheme: TextTheme(bodyText1: TextStyle(color: Colors.black))),
+      home: new InstaHome(),
+    );
+  }
+}
